0515b45a2d56415f20541fc4a50c0ebd920f86dd
Mauricio Graciano <mau.graci@gmail.com>
2022-07-27 17:54:01 -0500

    Setup new application "Dekstop" (#6)
    * Rename existing application "Desktop" to "Frontend"

--- packages/desktop/jest.config.ts
+++ packages/desktop/jest.config.ts
@@ -1,21 +1,16 @@
-module.exports = {
+/* eslint-disable */
+export default {
   displayName: 'desktop',
   preset: '../../jest.preset.js',
-  transform: {
-    '.*.(vue)$': '@vue/vue2-jest',
-    '^.+.ts$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'js', 'vue', 'json'],
-  coverageDirectory: '../../coverage/packages/desktop',
-  collectCoverageFrom: [
-    '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue',
-  ],
-  moduleNameMapper: {
-    '^vue$': 'vue/dist/vue.common.js',
-  },
   globals: {
-    'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' },
-    'vue-jest': { tsConfig: 'packages/desktop/tsconfig.spec.json' },
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
   },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/desktop',
 };
